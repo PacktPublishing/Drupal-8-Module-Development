f1ce342554dc7c1405562de0087bbb94122051e3
upchuk <danny.sipos@gmail.com>
2017-06-08 10:24:18 +0300

    Refactoring importer plugin manager to create instance based on config ID

--- products/src/Plugin/ImporterManager.php
+++ products/src/Plugin/ImporterManager.php
@@ -2,14 +2,22 @@
 
 namespace Drupal\products\Plugin;
 
+use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\Core\Plugin\DefaultPluginManager;
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\products\Entity\ImporterInterface;
+use Drupal\products\Plugin\ImporterInterface as ImporterPluginInterface;
 
 /**
  * Provides the Importer plugin manager.
  */
 class ImporterManager extends DefaultPluginManager {
+
+  /**
+   * @var \Drupal\Core\Entity\EntityTypeManager
+   */
+  protected $entityTypeManager;
 
   /**
    * ImporterManager constructor.
@@ -21,11 +29,31 @@ class ImporterManager extends DefaultPluginManager {
    *   Cache backend instance to use.
    * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
    *   The module handler to invoke the alter hook with.
+   * @param \Drupal\Core\Entity\EntityTypeManager $entityTypeManager
    */
-  public function __construct(\Traversable $namespaces, CacheBackendInterface $cache_backend, ModuleHandlerInterface $module_handler) {
+  public function __construct(\Traversable $namespaces, CacheBackendInterface $cache_backend, ModuleHandlerInterface $module_handler, EntityTypeManager $entityTypeManager) {
     parent::__construct('Plugin/Importer', $namespaces, $module_handler, 'Drupal\products\Plugin\ImporterInterface', 'Drupal\products\Annotation\Importer');
 
     $this->alterInfo('products_importer_info');
     $this->setCacheBackend($cache_backend, 'products_importer_plugins');
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * Creates an instance of ImporterInterface plugin based on the ID of a
+   * configuration entity.
+   *
+   * @param $id
+   *   Configuration entity ID
+   *
+   * @return null|ImporterPluginInterface
+   */
+  public function createInstanceFromConfig($id) {
+    $config = $this->entityTypeManager->getStorage('importer')->load($id);
+    if (!$config instanceof ImporterInterface) {
+      return NULL;
+    }
+
+    return $this->createInstance($config->getPluginId(), ['config' => $config]);
   }
 }
