91e114a074a9588070977a006e151db64a96bc0d
upchuk <danny.sipos@gmail.com>
2017-08-01 10:27:29 +0200

    Cache dependency on the hello world access checker

--- hello_world/src/Access/HelloWorldAccess.php
+++ hello_world/src/Access/HelloWorldAccess.php
@@ -35,6 +35,9 @@ class HelloWorldAccess implements AccessInterface {
    */
   public function access(AccountInterface $account) {
     $salutation = $this->configFactory->get('hello_world.custom_salutation')->get('salutation');
-    return in_array('editor', $account->getRoles()) && $salutation != "" ? AccessResult::forbidden() : AccessResult::allowed();
+    $access = in_array('editor', $account->getRoles()) && $salutation != "" ? AccessResult::forbidden() : AccessResult::allowed();
+    $access->addCacheableDependency($salutation);
+    $access->addCacheableDependency($account);
+    return $access;
   }
 }
