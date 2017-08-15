2cd7f6b97973d14e6d104a65a470bdfc0547eafd
upchuk <danny.sipos@gmail.com>
2017-08-15 17:36:31 +0200

    Players controller route with paged table

---
+++ sports/src/Controller/SportsController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\sports\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Connection;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Controller for the Sports module
+ */
+class SportsController extends ControllerBase {
+
+  /**
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * SportsController constructor.
+   *
+   * @param \Drupal\Core\Database\Connection $database
+   */
+  public function __construct(Connection $database) {
+    $this->database = $database;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('database')
+    );
+  }
+
+  /**
+   * Renders a table of players.
+   */
+  public function players() {
+    $limit = 5; // The number of items per page.
+    $query = $this->database->select('players', 'p')
+      ->fields('p')
+      ->extend('\Drupal\Core\Database\Query\PagerSelectExtender')
+      ->limit($limit);
+    $result = $query->execute()->fetchAll();
+    $header = [$this->t('Name')];
+    $rows = [];
+
+    foreach ($result as $row) {
+      $rows[] = [
+        $row->name
+      ];
+    }
+
+    $build = [];
+    $build[] = [
+      '#theme' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+    ];
+    $build[] = [
+      '#type' => 'pager'
+    ];
+
+    return $build;
+  }
+}
