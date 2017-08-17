e77fc96dc0c88c3a8579fc27050ef5249f570e8d
upchuk <danny.sipos@gmail.com>
2017-08-17 10:04:04 +0200

    Custom views filter

---
+++ sports/src/Plugin/views/filter/TeamFilter.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\sports\Plugin\views\filter;
+
+use Drupal\Core\Database\Connection;
+use Drupal\views\Plugin\views\filter\InOperator;
+use Drupal\views\ViewExecutable;
+use Drupal\views\Plugin\views\display\DisplayPluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Filter class which filters by the available teams.
+ *
+ * @ViewsFilter("team_filter")
+ */
+class TeamFilter extends InOperator {
+
+  /**
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * Constructs a Bundle object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, Connection $database) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->database = $database;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('database')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function init(ViewExecutable $view, DisplayPluginBase $display, array &$options = NULL) {
+    parent::init($view, $display, $options);
+    $this->valueTitle = t('Teams');
+    $this->definition['options callback'] = [$this, 'getTeams'];
+  }
+
+  /**
+   * Generates the list of teams that can be used in the filter.
+   */
+  public function getTeams() {
+    $result = $this->database->query("SELECT name FROM {teams}")->fetchAllAssoc('name');
+    if (!$result) {
+      return [];
+    }
+
+    $teams = array_keys($result);
+    return array_combine($teams, $teams);
+  }
+}
